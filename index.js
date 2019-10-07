5255380061eb7afd331e2e8ae8ad9d90893f1c30
Megan Vogler <mmphilmon@gmail.com>
2019-10-07 17:58:55 -0500

    first word function

---
+++ index.js
@@ -0,0 +1,11 @@
+var words = require("./word.js");
+
+// Randomly selects a word and uses the Word constructor to store it
+// Prompts the user for each guess and keeps track of the user's remaining guesses
+
+
+
+
+
+// Letter.js should not require any other files.
+// Word.js should only require Letter.js
--- word.js
+++ word.js
@@ -1,1 +1,33 @@
 var letters = require("./letter.js");
+
+function Word (newLetters) {
+    this.newLetters = newLetters;
+
+    // An array of new Letter objects representing the letters of the underlying word
+    var newLetters = [];
+
+    // A function that returns a string representing the word. This should call the 
+    // function on each letter object (the first function defined in Letter.js) that 
+    // displays the character or an underscore and concatenate those together.
+    this.toString = function() {
+        for (var i = 0; i < newLetters.length; i++){
+            var letter = new letters(newLetters[i]);
+            this.newLetters.push(letter);
+        }
+
+    };
+
+    // A function that takes a character as an argument and calls the guess function 
+    // on each letter object (the second function defined in Letter.js)
+    this.checkLetters = function() {
+
+    };
+}
+
+// Exporting the Letter Constructor 
+module.exports = Word;
+
+
+
+
+
